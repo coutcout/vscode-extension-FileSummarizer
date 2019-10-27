5f8a4dc1ef96cb8b77304d14bf2f88b9ff858a03
Valentin C <vcouturier0@gmail.com>
2019-10-28 00:07:46 +0100

    [AZURE] test script

---
+++ publish.js
@@ -0,0 +1,22 @@
+const execSync = require('child_process').execSync;
+
+const gitToken = process.env.GIT_TOKEN || "wrong_token";
+const vsceToken = process.env.VSCE_TOKEN || "wrong_token";
+
+const repoURL = "https://" + gitToken + "@github.com/coutcout/vscode-extension-FileSummarizer.git";
+const gitCmd = "git push --follow-tags" + repoURL + "origin/master";
+
+const vsceCmd = "vsce publish -p " + vsceToken;
+
+const npmCmd = "npm install";
+
+console.log(">>> Installing dependencies");
+execSync(npmCmd, {stdio:[0, 1, 2]});
+
+console.log(">>> Publishing");
+execSync(vsceCmd, {stdio:[0, 1, 2]});
+
+console.log(">>> Pushing tags on git repo");
+execSync(gitCmd, {stdio:[0, 1, 2]});
+
+
