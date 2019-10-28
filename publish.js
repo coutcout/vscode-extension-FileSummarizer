558e2850f943a1ab120c4ab51c39499a16d7b4e7
Valentin C <vcouturier0@gmail.com>
2019-10-28 01:33:26 +0100

    [AZURE] test script

--- publish.js
+++ publish.js
@@ -1,4 +1,5 @@
 const execSync = require('child_process').execSync;
+const package = require('./package.json');
 
 const gitToken = process.argv[3] || "wrong_token";
 const vsceToken = process.argv[2] || "wrong_token";
@@ -16,7 +17,14 @@ execSync(npmCmd, {stdio:[0, 1, 2]});
 console.log(">>> Publishing");
 execSync(vsceCmd, {stdio:[0, 1, 2]});
 
-console.log(">>> Pushing tags on git repo");
-execSync(gitCmd, {stdio:[0, 1, 2]});
+const version = package.version;
+if(version){
+    console.log(">>> Creating tag " + version);
+    execSync("git tag " + version + " -m " + version);
+
+    console.log(">>> Pushing tags on git repo");
+    execSync(gitCmd, {stdio:[0, 1, 2]});
+}
+
 
 
